cf9638f666c3b858c5ccde3fccec6751e83dcbf7
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-07-30 12:40:56 -0300

    feat: configura rotas no App.jsx para renderizar a página de produtos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './assets/css/tailwind.css';
 
+import ProductsPage from './pages/ProductsPage';
+
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
