72f293963cc8f1a8666663dab11a187c4fbe2fff
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-08-15 12:32:06 -0300

    feat: adiciona rota para AddProductPage e NavBar no App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './assets/css/tailwind.css';
 
+import NavBar from './components/NavBar';
+import AddProductPage from './pages/AddProductPage';
 import ProductsPage from './pages/ProductsPage';
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
+
       <Routes>
+        <Route path="/" element={null} />
         <Route path="/products" element={<ProductsPage />} />
+        <Route path="/addProduct" element={<AddProductPage />} />
       </Routes>
     </BrowserRouter>
   );
