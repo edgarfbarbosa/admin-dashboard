5892fe9a8553c1d0aa66b4eeb605047241ff7bfa
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-08-16 12:08:27 -0300

    refactor: move BrowserRouter para main.jsx e ajusta layout em App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import './assets/css/tailwind.css';
 
@@ -8,15 +8,17 @@ import ProductsPage from './pages/ProductsPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <div className="flex">
       <NavBar />
 
-      <Routes>
-        <Route path="/" element={null} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/addProduct" element={<AddProductPage />} />
-      </Routes>
-    </BrowserRouter>
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={null} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/addProduct" element={<AddProductPage />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
