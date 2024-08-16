9c61383ed7727ab0c523d36f79cb857cf43c56b0
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-08-16 12:10:00 -0300

    feat: adiciona biblioteca Lucide para ícones e atualiza NavBar com novos ícones e layout

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,18 +1,30 @@
 import { Link } from 'react-router-dom';
+import { AlignJustify, User, Package, PackagePlus } from 'lucide-react';
 
 const NavBar = () => {
   return (
-    <header>
-      <nav>
-        <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+    <header className="h-screen w-36 flex flex-col items-center">
+      <div className="my-7">
+        <button className="btn btn-square">
+          <AlignJustify />
+        </button>
+      </div>
+      <nav className="h-full flex items-start">
+        <ul className="menu space-y-7">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">
+              <User />
+            </Link>
           </li>
           <li>
-            <Link to="/products">Produtos</Link>
+            <Link to="/products">
+              <Package />
+            </Link>
           </li>
           <li>
-            <Link to="/addProduct">Adicionar Produto</Link>
+            <Link to="/addProduct">
+              <PackagePlus />
+            </Link>
           </li>
         </ul>
       </nav>
