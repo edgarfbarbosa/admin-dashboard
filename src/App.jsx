256a64e0a82efc9cea645ed24bf2e752a6c8e5a1
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-09-02 17:57:53 -0300

    refactor: atualiza estrutura do App para usar Header e atualiza rota para registro de produto

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,19 @@ import { Routes, Route } from 'react-router-dom';
 
 import './assets/css/tailwind.css';
 
-import NavBar from './components/NavBar';
-import AddProductPage from './pages/AddProductPage';
+import Header from './components/Header';
+import ProductRegisterPage from './pages/ProductRegisterPage';
 import ProductsPage from './pages/ProductsPage';
 
 function App() {
   return (
     <div className="flex">
-      <NavBar />
+      <Header />
 
       <main className="flex-grow">
         <Routes>
-          <Route path="/" element={null} />
           <Route path="/products" element={<ProductsPage />} />
-          <Route path="/addProduct" element={<AddProductPage />} />
+          <Route path="/register" element={<ProductRegisterPage />} />
         </Routes>
       </main>
     </div>
